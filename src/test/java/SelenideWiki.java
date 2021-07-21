3da35ea2023c543a54b5ff3b63c7132fed34b393
Alexander Terentev <33878295+winnerrr@users.noreply.github.com>
2021-07-21 12:09:38 +0300

    Commit for HW8

--- src/test/java/SelenideWiki.java
+++ src/test/java/SelenideWiki.java
@@ -2,18 +2,19 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideWiki {
-
+    // Comment HW8
     @Test
     void softAssertionsLinkTest() {
-            open("https://github.com/selenide/selenide");
-            $("[data-content=Wiki]").click();
+        open("https://github.com/selenide/selenide");
+        $("[data-content=Wiki]").click();
 
-            $("#wiki-pages-filter").setValue("SoftAssertions");
-            $(byText("SoftAssertions")).click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(byText("SoftAssertions")).click();
 
-            $("[id=wiki-body]").shouldHave(text("Using JUnit5 extend test class"));
+        $("[id=wiki-body]").shouldHave(text("Using JUnit5 extend test class"));
     }
 }
